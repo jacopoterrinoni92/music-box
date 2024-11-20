ab19eb641f4b4547a0b61e8058865d3c82ce2499
jacopoterrinoni92 <jacopo.terrinoni92@gmail.com>
2024-11-20 14:32:36 +0100

    first commit

---
+++ mixer.py
@@ -0,0 +1,113 @@
+import pygame
+import pathlib
+
+class Mixer:
+    """ This class is used to manage pygame.Mixer"""
+
+    def __init__(
+        self, 
+        frequency=44100, 
+        size=-16, 
+        channels=2, 
+        buffer=512, 
+        devicename=None, 
+        allowedchanges=AUDIO_ALLOW_FREQUENCY_CHANGE | AUDIO_ALLOW_CHANNELS_CHANGE
+    ):
+
+        self.frequency = frequency
+        self.size = size
+        self.channels = channels
+        self.buffer = buffer
+        self.devicename = devicename
+        self.allowedchanges = allowedchanges
+
+        self.mixer = pygame.mixer.pre_init(frequency, size, channels, buffer, devicename, allowedchanges)
+        self.mixer = pygame.mixer.init()
+
+        self.sound = None
+        self.volume = 0.0
+
+    """
+    This will uninitialize pygame.mixerpygame module for loading and playing sounds. 
+    All playback will stop and any loaded Sound objects may not be compatible with the mixer if it is reinitialized later.
+    """
+    def quit(self) -> None:
+        self.mixer.quit()
+
+    """
+    This will stop all playback of all active mixer channels.
+    """
+    def stop(self) -> None:
+        self.mixer.stop()
+
+    """
+    This will temporarily stop all playback on the active mixer channels.
+     The playback can later be resumed with pygame.mixer.unpause()
+    """
+    def pause(self) -> None:
+        self.mixer.pause()
+
+    """
+    This will resume all active sound channels after they have been paused.
+    """
+    def unpause(self) -> None:
+        self.mixer.unpause()
+
+    """
+    Returns True if the mixer is busy mixing any channels. 
+    If the mixer is idle then this return False.
+    """
+    def get_busy(self) -> Bool:
+        self.mixer.get_busy()
+
+    """
+    Load a new sound buffer from a filename, a python file object or a readable buffer object.
+    args:
+        file_path: The path of the mp3 song that should be played.
+    """
+    def sound(self, file_path: pathlib.Path) -> None:
+        self.sound = pygame.mixer.Sound(file=file_path)
+
+    """
+    Begin playback of the Sound (i.e., on the computer's speakers) on an available Channel.
+    args:
+        loops: controls how many times the sample will be repeated after being played the first time.
+        maxtime: can be used to stop playback after a given number of milliseconds.
+        fade_ms: make the sound start playing at 0 volume and fade up to full volume over the time given. 
+    """
+    def sound_play(self, loops=0, maxtime=0, fade_ms=0) -> Channel:
+        return self.sound.play(loops=loops, maxtime=maxtime, fade_ms=fade_ms)
+
+    """
+    This will stop the playback of this Sound on any active Channels.
+    """
+    def sound_stop(self) -> None:
+        self.sound.stop()
+
+    """
+    This will stop playback of the sound after fading it out over the time argument in milliseconds.
+    args:
+        time: The amount of time for the fadeout.
+    """
+    def sound_fadeout(self, time=0) -> None:
+        self.sound.fadeout(time=time)
+
+    """
+    Set the playback volume for this Sound
+    args:
+        volume: Volume in the range of 0.0 to 1.0 (inclusive)
+    """
+    def sound_set_volume(self, volume: float) -> None:
+        self.sound.set_volume(value=volume)
+
+    """
+    Return a value from 0.0 to 1.0 representing the volume for this Sound.
+    """
+    def sound_get_volume(self) -> float:
+        return self.sound.get_volume()
+
+    
+
+
+    
+
